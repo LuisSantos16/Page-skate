e0b789766544b85de9e50bde1083924d1fec1d45
luissantos16 <lmsr.1601@gmail.com>
2023-06-23 13:51:23 -0500

    page skate finish update6

--- js/java.js
+++ js/java.js
@@ -6,3 +6,33 @@ function llamar(){
     let listabar=document.getElementById("lista-bar");
     listabar.querySelector("lista-bar .listabar").classList.toggle;
 }
+
+//sombreado
+
+window.addEventListener("scroll",Scroll);
+function Scroll(){
+    let header=document.getElementById("listaid");
+    let posy=window.pageYOffset;
+    if (posy>100){
+        header.style.background="rgba(0, 0, 0, 0.80)";
+        header.style.transition="0.5s"
+    }
+    else{
+        header.style.background="rgba(0, 0, 0, 0.20)";
+        header.style.transition="0.5s"
+    }
+}
+
+window.addEventListener("scroll",Scroll);
+function Scroll(){
+    let header=document.getElementById("conta-menu");
+    let posy=window.pageYOffset;
+    if (posy>100){
+        header.style.background="rgba(0, 0, 0, 0.80)";
+        header.style.transition="0.5s"
+    }
+    else{
+        header.style.background="none";
+        header.style.transition="0.5s"
+    }
+}
