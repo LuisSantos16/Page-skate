1f6bb02b09a0a5041917c66aa32d58804da2efba
luissantos16 <lmsr.1601@gmail.com>
2023-06-23 12:09:15 -0500

    page with responsive

---
+++ js/java.js
@@ -0,0 +1,8 @@
+//menu
+
+function llamar(){
+    let menuhamburguesa=document.getElementById("listaid");
+        menuhamburguesa.classList.toggle("lista-open")
+    let listabar=document.getElementById("lista-bar");
+    listabar.querySelector("lista-bar .listabar").classList.toggle;
+}
